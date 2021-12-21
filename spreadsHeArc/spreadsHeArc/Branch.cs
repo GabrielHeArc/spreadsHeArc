5e819f3b1aa776f9633255223873a429d1951917
Fasano Gabriel <gabriel.fasano@he-arc.ch>
2021-12-21 16:15:13 +0100

    Add new classes

---
+++ spreadsHeArc/spreadsHeArc/Branch.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace spreadsHeArc
+{
+    public class Branch
+    {
+        string Name;
+        Dictionary<float, int> DictRating = new Dictionary<float, int>();
+    }
+}
---
+++ spreadsHeArc/spreadsHeArc/Module.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spreadsHeArc
+{
+    public class Module
+    {
+        string Name;
+        Dictionary<Branch, int> DictBranch = new Dictionary<Branch, int>();
+    }
+}
